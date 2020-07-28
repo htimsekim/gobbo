01f9c3f698e7a998efd1a8fd47acb9712faa8e1a
Mike <mls.mikesmith@gmail.com>
2020-07-28 18:55:42 -0500

    optional full screen second monitor
    just lazy dev stuff

--- src/Main/Game.gd
+++ src/Main/Game.gd
@@ -3,5 +3,11 @@ extends Node
 const levelResource = "res://src/levels/gobtown.tscn"
 
 func _ready():	
-	#OS.window_fullscreen = true
+	full_screen() #comment this code out to run the game in a window
 	Global.goto_scene(levelResource, "level/spawns/spawna")
+
+
+func full_screen():
+	OS.set_current_screen(0)
+	OS.window_fullscreen = true
+	OS.set_borderless_window(true)
