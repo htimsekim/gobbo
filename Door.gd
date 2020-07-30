5e6e7414fd359ea1bcf265fdacc0b781dd8676bb
Mike <mls.mikesmith@gmail.com>
2020-07-30 14:17:04 -0500

    doors work now
    push up to enter

--- Door.gd
+++ Door.gd
@@ -3,5 +3,23 @@ extends Area2D
 export(String, FILE, "*.tscn") var target_stage
 export(String) var target_spawn
 
+var in_door = false
+
+func _process(delta):
+	if Input.is_action_pressed("move_up") and in_door:
+		Global.goto_scene(target_stage, target_spawn)
+		get_tree().call_group("projectile","queue_free")
+		print("trying")
+
+func _on_Door_body_entered(body):
+	if "plyrInst" in body.name:
+		in_door = true
+		print("in")
 
 
+
+
+func _on_Door_body_exited(body):
+	if "plyrInst" in body.name:
+		in_door = false
+		print("out")
