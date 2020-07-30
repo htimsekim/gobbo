0fa79cec90a9b4864ea1c90dfa50cea671f578cc
Mike <mls.mikesmith@gmail.com>
2020-07-30 13:12:05 -0500

    tilemap cleanup, door object
    added door object in group "door", push up to enter

---
+++ Door.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+export(String, FILE, "*.tscn") var target_stage
+export(String) var target_spawn
+
+
+
--- src/Objects/link.gd
+++ src/Objects/link.gd
@@ -3,8 +3,6 @@ extends Area2D
 export(String, FILE, "*.tscn") var target_stage
 export(String) var target_spawn
 
-func _enter_tree():
-	self.visible = false
 
 func _on_ChangeStage_body_entered(body):
 	if "plyrInst" in body.name:
