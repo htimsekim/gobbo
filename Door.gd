0c1a6b6e13581952e4441bea3a0460b170b4babe
Mike <mls.mikesmith@gmail.com>
2020-07-30 22:25:58 -0500

    up arrow indicator for doors

--- Door.gd
+++ Door.gd
@@ -3,6 +3,8 @@ extends Area2D
 export(String, FILE, "*.tscn") var target_stage
 export(String) var target_spawn
 
+var doorIndicator = Global.get_node("plyrInst/Sprite/DoorIndicator")
+var indicAnimation = Global.get_node("plyrInst/Sprite/DoorIndicator/AnimationPlayer")
 var in_door = false
 
 func _process(delta):
@@ -12,7 +14,10 @@ func _process(delta):
 func _on_Door_body_entered(body):
 	if "plyrInst" in body.name:
 		in_door = true
+		indicAnimation.play("up")
+		doorIndicator.visible = true
 
 func _on_Door_body_exited(body):
 	if "plyrInst" in body.name:
 		in_door = false
+		doorIndicator.visible = false
