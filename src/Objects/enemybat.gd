dfa04ff4f13021ddbc628da4bc0778fb772d238a
sarrisa <sarrisamk@gmail.com>
2020-08-17 17:55:16 -0500

    enemy

--- src/Objects/enemybat.gd
+++ src/Objects/enemybat.gd
@@ -1,11 +1,13 @@
-extends RigidBody2D
+class_name Enemy
+extends Character
+
 onready var player = Global.get_node("plyrInst")
 onready var direction = Vector2()
 onready var distance = 0
 onready var follow = false
 
 func _physics_process(delta): # Called every frame. _delta isn't used
-	direction = player.position - self.position # get direction / distance of player
+	direction = player.position - position # get direction / distance of player
 	# Determine whether to flip enemy image to face player
 	if direction.x > 0:
 		self.get_node("Sprite").set_flip_h(1) # Set Enemy facing right
@@ -15,13 +17,13 @@ func _physics_process(delta): # Called every frame. _delta isn't used
 	distance = sqrt(direction.x * direction.x + direction.y * direction.y) 	# calculate distance from player
 
 	# If distance less than 150 then start Enemy following player
-	if distance < 150 and player.knockback == false:
+	if distance < 100 and player.knockback == false:
 		follow = true
 	else:
 		follow = false
 	
 	if follow == true:
-		position += direction.normalized() * player.speed * delta #move enemy to player
+		move_and_collide(direction.normalized() * speed * delta) #move enemy to player
 		$AnimationPlayer.play("move")
 		
 func damage(amount): #called from projectile amount is 1
