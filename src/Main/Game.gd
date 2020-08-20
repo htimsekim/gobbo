f4e93df040f733486bf0893fe91055eef0f07b1e
Mike <mls.mikesmith@gmail.com>
2020-08-20 13:20:37 -0500

    inside tree

--- src/Main/Game.gd
+++ src/Main/Game.gd
@@ -5,7 +5,7 @@ const levelResource = "res://src/levels/forest.tscn"
 
 func _ready():	
 	full_screen() #comment this code out to run the game in a window
-	Global.goto_scene(levelResource, "level/spawns/spawna")
+	Global.goto_scene(levelResource, "level/spawns/spawn1")
 
 func full_screen():
 	OS.set_current_screen(0)
