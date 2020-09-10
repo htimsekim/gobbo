fd4bedc43ca5335e9eb4eb63cdd31bc97848442b
Mike <mls.mikesmith@gmail.com>
2020-09-10 18:31:46 -0500

    assets, npcs

---
+++ src/npc/goblin-npc.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+export(String) var npc_animation
+var rng = RandomNumberGenerator.new()
+
+func _ready():
+	var goblins = $AnimationPlayer.get_animation_list()
+	rng.randomize()
+	$AnimationPlayer.set_speed_scale(rng.randf_range(0.1, 0.2))
+	$AnimationPlayer.play(goblins[randi() % goblins.size()])
+	
