26375f1e7db0be957565e19018a7bbbbcd971c08
Mike <mls.mikesmith@gmail.com>
2020-09-18 14:42:31 -0500

    assets/levels

--- Door.gd
+++ Door.gd
@@ -8,14 +8,19 @@ var indicAnimation = Global.get_node("plyrInst/Sprite/DoorIndicator/AnimationPla
 var in_door = false
 
 func _process(_delta):
-	if Input.is_action_just_pressed("move_up") and in_door:
+	if in_door and Global.player.is_on_floor():
+		indicAnimation.play("up")
+		doorIndicator.visible = true
+		
+	if Input.is_action_just_pressed("move_up") and in_door and Global.player.is_on_floor():
 		Global.goto_scene(target_stage, target_spawn)
+	
 
 func _on_Door_body_entered(body):
 	if "plyrInst" in body.name:
 		in_door = true
-		indicAnimation.play("up")
-		doorIndicator.visible = true
+		
+	
 
 func _on_Door_body_exited(body):
 	if "plyrInst" in body.name:
