41883e770d24473448678b111393ca7a9088b36a
Mike <mls.mikesmith@gmail.com>
2020-09-22 08:49:49 -0500

    asset

--- src/npc/goblin-npc.gd
+++ src/npc/goblin-npc.gd
@@ -5,7 +5,9 @@ var rng = RandomNumberGenerator.new()
 
 func _ready():
 	var goblins = $AnimationPlayer.get_animation_list()
+	
 	rng.randomize()
 	$AnimationPlayer.set_speed_scale(rng.randf_range(0.1, 0.2))
 	$AnimationPlayer.play(goblins[randi() % goblins.size()])
 	
+	print(goblins[randi() % goblins.size()])
