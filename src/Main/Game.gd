dde30f72f60ade1a4a3f22340032545f37a53d97
Mike <mls.mikesmith@gmail.com>
2020-09-29 11:56:39 -0500

    now it's always on the right screen
    kiss my asssssssss

--- src/Main/Game.gd
+++ src/Main/Game.gd
@@ -1,13 +1,24 @@
 extends Node
 
-#const levelResource = "res://src/levels/demo.tscn"
-const levelResource = "res://src/levels/gobtown-prequake.tscn"
+const levelResource = "res://src/levels/demo.tscn"
+#const levelResource = "res://src/levels/gobtown-prequake.tscn"
+
+var username
+
 
 func _ready():	
+	if OS.has_environment("USERNAME"):
+		username = OS.get_environment("USERNAME")
+	else:
+		username = "player"
+	
 	full_screen() #comment this code out to run the game in a window
 	Global.goto_scene(levelResource, "level/spawns/spawn1")
 
 func full_screen():
-	OS.set_current_screen(0)
+	if username == "mike":
+		OS.set_current_screen(0)
+	else:
+		OS.set_current_screen(2)
 	OS.window_fullscreen = true
 	OS.set_borderless_window(true)
