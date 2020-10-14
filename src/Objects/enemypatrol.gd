ca2f0d214dabce12402bca5ac262e864d0444d60
sarrisa <sarrisamk@gmail.com>
2020-10-14 15:19:37 -0500

    enemy raycast

--- src/Objects/enemypatrol.gd
+++ src/Objects/enemypatrol.gd
@@ -2,14 +2,20 @@ class_name enemypatrol
 extends KinematicBody2D
 
 onready var direction = Vector2(1,0)
+onready var platform_detector = $Hurtbox
 export(int) var patrolDistance # walking distance of enemy
 export(int) var movespeed # walking distance of enemy
 var start_pos = 0
 
 func _ready():
 	start_pos = position.x
+	platform_detector = platform_detector.get_node("FloorDetector")
 	
 func _physics_process(_delta): # Called every frame. _delta isn't used
+	var is_on_platform = platform_detector.is_colliding()
+	if is_on_platform == false:
+		direction.x *= -1
+		
 	if direction.x == 1: #flip character in right direction and patrol him
 		$Sprite.flip_h = false # Set Enemy facing right
 		if position.x < start_pos + patrolDistance:
