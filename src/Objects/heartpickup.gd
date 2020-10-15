f697328a2985e485db3a7a2725edc60163d82a6b
sarrisa <sarrisamk@gmail.com>
2020-10-15 12:37:36 -0500

    bullet max

--- src/Objects/heartpickup.gd
+++ src/Objects/heartpickup.gd
@@ -1,5 +1,4 @@
 extends Area2D
-class_name heartpickup
 
 func _on_heartpickup_body_entered(_body):
 	Global.player.get_node("TextureProgress").value = Global.player.get_node("TextureProgress").value + Global.player.get_node("TextureProgress").step
