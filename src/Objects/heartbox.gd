4f298a05d4a2775e7c68605aac8fae28438399ff
sarrisa <sarrisamk@gmail.com>
2020-10-15 13:44:30 -0500

    bullethealthLevelup

--- src/Objects/heartbox.gd
+++ src/Objects/heartbox.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-func _on_bulletbox_body_entered(body):
+func _on_heartbox_body_entered(body):
 	if body.is_in_group("plyr"):	
 		Global.player.get_node("TextureProgress").max_value += 1 
 		Global.player.get_node("TextureProgress").value = Global.player.get_node("TextureProgress").max_value
--- src/levels/UI.gd
+++ src/levels/UI.gd
@@ -10,17 +10,19 @@ func update_maxhealth(maxvalue):
 func update_maxbullet(maxvalue):
 	for h in 10:
 		get_child(h).visible = false
+		get_child(h).get_child(1).play("full")	
 	for h in maxvalue:
 		get_child(h).visible = true
-		
+	reload = false
+	
 func update_health(value):
 	for i in get_child_count():
 		if value >= i + 1 :
-			get_child(i).frame = 0 #full
+			get_child(i).get_child(0).frame = 0 #full
 		elif value > i:  
-			get_child(i).frame = 1 #half full
+			get_child(i).get_child(0).frame = 1 #half full
 		else:
-			get_child(i).frame = 2 #empty
+			get_child(i).get_child(0).frame = 2 #empty
 	
 func update_bullet(value):
 	if reload == true and get_node("../reloadTimer").time_left == 0: #if we need to reload and timer hasn't started, start it
@@ -29,13 +31,13 @@ func update_bullet(value):
 	for i in get_child_count():
 		if value == -1: #reload
 			reload = true
-			get_child(i).get_child(0).play("reload")
+			get_child(i).get_child(1).play("reload")
 
 		if reload == false:
 			if value >= i + 1:
-				get_child(i).get_child(0).play("full")
+				get_child(i).get_child(1).play("full")
 			else:
-				get_child(i).get_child(0).play("empty")
+				get_child(i).get_child(1).play("empty")
 
 func _on_reloadTimer_timeout():
 	Global.player.get_node("BulletHealth").value = Global.player.get_node("BulletHealth").max_value
