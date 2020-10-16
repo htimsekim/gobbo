ff6f05062c5abcfbd9c7ac409e068bb25ccc32b7
sarrisa <sarrisamk@gmail.com>
2020-10-16 14:27:07 -0500

    mixed changes

--- src/Main/Global.gd
+++ src/Main/Global.gd
@@ -28,16 +28,12 @@ func goto_scene(path, spawn): #main scene switcher call - Global.goto_scene("res
 	get_tree().call_group("projectile","queue_free")
 	
 func _deferred_goto_scene(path, spawn: String):
-
-
-
-
-	#current_scene.free() # It is now safe to remove the current scene
 	get_tree().get_root().remove_child(current_scene)
 	previous_scene_x2 = previous_scene
 	previous_scene = current_scene
 	previous_scene_x2_path = previous_scene_path
 	previous_scene_path = current_scene.filename
+	
 	if path == previous_scene_x2_path:
 		get_tree().get_root().add_child(previous_scene_x2)
 		camera = previous_scene_x2.get_node("level/Camera2D")
@@ -45,7 +41,7 @@ func _deferred_goto_scene(path, spawn: String):
 		camera.position = previous_scene_x2.get_node(str(spawn)).position #set camera spawn
 		current_scene = previous_scene_x2
 	else:
-		#remove previous_scene_x2
+		if previous_scene_x2: previous_scene_x2.free()
 		var s = ResourceLoader.load(path) # Load the new scene.
 		current_scene = s.instance() # Instance the new scene.
 		get_tree().get_root().add_child(current_scene) # Add it to the active scene, as child of root.
@@ -55,11 +51,3 @@ func _deferred_goto_scene(path, spawn: String):
 		camera = current_scene.get_node("level/Camera2D")
 		player.position = current_scene.get_node(str(spawn)).position #set plyr spawn
 		camera.position = current_scene.get_node(str(spawn)).position #set camera spawn
-		
-	print("path = ", path)
-	print("prevpath = ", previous_scene_path)
-	print("prevpathx2 = ", previous_scene_x2_path)
-	print("prevx2 = ", previous_scene_x2)
-	print("prev = ", previous_scene)
-	print("current = ", current_scene)
-	print(" ")
--- src/Objects/saveBox.gd
+++ src/Objects/saveBox.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 func _on_saveBox_body_entered(body):
-	var confirm = load("res://src/UI/saveConfirm.tscn").instance()
-	self.add_child(confirm)
-	get_tree().paused = true
+	if body.is_in_group("plyr"):
+		var confirm = load("res://src/UI/saveConfirm.tscn").instance()
+		self.add_child(confirm)
+		get_tree().paused = true
