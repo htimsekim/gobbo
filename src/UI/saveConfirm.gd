41948dd21a17938eae4e1be35043921e622f49b5
Mike <mls.mikesmith@gmail.com>
2020-10-20 11:23:53 -0500

    savebox config

--- src/UI/saveConfirm.gd
+++ src/UI/saveConfirm.gd
@@ -2,15 +2,15 @@ extends CanvasLayer
 
 onready var conChoice = ""
 func _ready():
-	get_node("NinePatchRect/Sprite").position = Vector2(20,51)
+	get_node("Sprite").position = Vector2(160,406)
 	
 func _process(_delta):
 	if Input.is_action_pressed("move_right"): 
 		conChoice = "Yes"
-		get_node("NinePatchRect/Sprite").position = Vector2(60,51)
+		get_node("Sprite").position = Vector2(485,406)
 	if Input.is_action_pressed("move_left"): 
 		conChoice = "No"
-		get_node("NinePatchRect/Sprite").position = Vector2(20,51)
+		get_node("Sprite").position = Vector2(160,406)
 	if Input.is_action_pressed("jump"): 
 		if conChoice == "Yes":
 			Global.save_game("GinaTest")
